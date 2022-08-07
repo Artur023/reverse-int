c3c10c49940eb0d1bd21e7530e5af5a56ee8fb52
Artur023 <artur.023@mail.ru>
2022-08-08 01:07:24 +0400

    feat: complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = n + ""; //'524'
+    if (n[0] !== "-") {
+        return Number(n.split("").reverse().join(""));
+    } else {
+        let n_WishOutMinus = n.slice(1, 4);
+        let reverseNumArr = n_WishOutMinus.split("").reverse();
+        return Number(reverseNumArr.join(''));
+    }
+};
